b5b726542c714fa7368a9099b7302c75cc5340a7
Asmae Mandour <amandour@e3r3p2.1337.ma>
2025-12-08 15:42:03 +0100

    client Class

---
+++ Client.cpp
@@ -0,0 +1,139 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amandour <amandour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 20:16:06 by amandour          #+#    #+#             */
+/*   Updated: 2025/12/01 21:03:31 by amandour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Client.hpp"
+
+Client::Client()
+{
+    _fd = 0;
+    _ipadd = "";
+    _username = "";
+    _nickname = "";
+    _hasPass = false;
+    _isRegistred = false;
+    _hasUser = false;
+    _hasNick = false;  
+}
+
+Client::~Client()
+{
+}
+
+Client::Client(int fd, std::string ipadd, std::string nick, std::string user)
+{
+    _fd = fd;
+    _ipadd = ipadd;
+    _username = user;
+    _nickname = nick;
+}
+
+Client &Client::operator=(const Client &src)
+{
+    if (this != &src)
+    {
+        _fd = src._fd;
+        _ipadd = src._ipadd;
+        _username = src._username;
+        _nickname = src._nickname;
+    }
+    return *this;
+}
+
+Client::Client(const Client &src)
+{
+    *this = src;
+}
+
+int            Client::getFd() const
+{
+    return _fd;
+}
+
+std::string    Client::getIpadd() const
+{
+    return _ipadd;
+}
+
+std::string &Client::getNickname()
+{
+    return _nickname;
+}
+
+std::string &Client::getUserName()
+{
+    return _username;
+}
+
+bool           Client::getHaspass() const
+{
+    return _hasPass;
+}
+
+void        Client::setFd(int fd)
+{
+    _fd = fd;
+}
+
+void        Client::setIpadd(std::string ipadd)
+{
+    _ipadd = ipadd;
+}
+
+void    Client::setUsername(std::string user_name)
+{
+    _username = user_name;
+}
+
+void Client::setNickname(std::string nick_name)
+{
+    _nickname = nick_name;
+}
+
+void Client::setHaspass(bool hasPass)
+{
+    _hasPass = hasPass;
+}
+
+void Client::setIsRegistred(bool is_registred)
+{
+    _isRegistred = is_registred;
+}
+
+bool Client::getIsRegistred() const
+{
+    return _isRegistred;
+}
+
+void Client::setHasUser(bool has_user)
+{
+    _hasUser = has_user;
+}
+
+bool Client::getHasUser() const
+{
+    return _hasUser;
+}
+
+void Client::setHasNick(bool has_nick)
+{
+    _hasNick = has_nick;
+}
+
+bool Client::getHasNick() const
+{
+    return _hasNick;
+}
+
+std::string &Client::getBuffer()
+{
+    return _buffer;
+}
---
+++ Client.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amandour <amandour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 20:21:07 by amandour          #+#    #+#             */
+/*   Updated: 2025/12/01 20:49:56 by amandour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Client
+{
+    private:
+        int        		_fd;
+        std::string		_ipadd;
+        std::string		_nickname;
+        std::string		_username;
+        bool       		_hasPass;
+        bool       		_isRegistred;
+        bool       		_hasUser;
+        bool       		_hasNick;
+        std::string		_buffer;
+    public:
+
+        Client();
+        Client(int fd, std::string ipadd, std::string nick, std::string user);
+        Client(const Client &src);
+        Client &operator=(const Client &src);
+        ~Client();
+		
+        //****** getters
+        int            getFd() const;
+        std::string    getIpadd() const;
+        std::string    &getUserName();
+        std::string    &getNickname();
+        bool           getHaspass() const;
+        bool           getIsRegistred() const;
+        bool           getHasUser() const;
+        bool           getHasNick() const;
+        std::string    &getBuffer();
+
+        //****** setters 
+
+        void        setFd(int fd);
+        void        setIpadd(std::string ipadd);
+        void        setUsername(std::string username);
+        void        setNickname(std::string nickname);
+        void        setHaspass(bool hasPass);
+        void        setIsRegistred(bool isregistred);
+        void        setHasUser(bool hasuser);
+        void        setHasNick(bool hasnick);
+}; 
